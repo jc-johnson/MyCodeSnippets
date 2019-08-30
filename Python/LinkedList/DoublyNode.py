86ead351b3a51700d7c35d562fa74e2e11ba136e
Jordan <jjohnson@myself.com>
2019-08-30 11:41:10 -0700

    doubly

---
+++ Python/LinkedList/DoublyNode.py
@@ -0,0 +1,5 @@
+class DoublyNode:
+    def __init__(self, val):
+        self.val = data
+        self.next = None
+        self.prev = None
--- Python/LinkedList/Node.py
+++ Python/LinkedList/Node.py
@@ -1,6 +1,29 @@
 class Node:
+    
+    # Constructor 
     def __init__(self,val):
         self.val = val
         self.next = None # the pointer initially points to nothing
         
         
+    def traverse(self):
+        node = self # start from the head node
+        while node != None:
+            print node.val # access the node value
+            node = node.next # move on to the next node
+            
+            
+    def main():
+        """ Main entry point of the app """
+        print("hello world")
+        
+        node1 = Node(12) 
+        node2 = Node(99) 
+        node3 = Node(37) 
+        node1.next = node2 # 12->99
+        node2.next = node3 # 99->37
+        # the entire linked list now looks like: 12->99->37
+
+    if __name__ == "__main__":
+        """ This is executed when run from the command line """
+        main()
