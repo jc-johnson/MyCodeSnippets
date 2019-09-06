8d49b73744166c0b5b9bc9bec1018b6bb81da3d2
Jordan <jjohnson@myself.com>
2019-09-06 13:51:53 -0500

    Linked list update

--- Python/LinkedList/FlatteningALinkedList.py
+++ Python/LinkedList/FlatteningALinkedList.py
@@ -3,7 +3,8 @@ class Node:
     # Constructor 
     def __init__(self,val):
         self.val = val
-        self.next = None # the pointer initially points to nothing
+        self.next = None # pointer to the next node in main list
+        self.down = None # pointer to next node in secondary/down list
         
     # Traverse a linked list 
     def traverse(self):
@@ -11,8 +12,25 @@ class Node:
         while node != None:
             print (node.val) # access the node value
             node = node.next # move on to the next node
+
+    # Traverse a linked list
+    @staticmethod
+    def traverseMain(self):
+        node = self # start from the head node
+        while node != None:
+            print (node.val) # access the node value
+            node = node.next # move on to the next node
+
+    # Traverse a linked list
+    @staticmethod
+    def traverseDown(self):
+        node = self # start from the head node
+        while node != None:
+            print (node.val) # access the node value
+            node = node.down # move on to the next node
        
     # returns the length of a list
+    @staticmethod
     def getListLength(self):
         if self is None:
             return 0
@@ -26,7 +44,8 @@ class Node:
         return counter
 
 
-    # print the nth node in a list 
+    # print the nth node in a list
+    @staticmethod
     def printNthNode(self, nodeNumber):
         if self is None:
             return
@@ -36,6 +55,9 @@ class Node:
         print (node.val)
 
 
+    # returns middle node in list of odd lenght
+    # returns middle two nodes in list of even length
+    @staticmethod
     def getListMiddle(self):
         if (self is None):
             return 0
@@ -52,9 +74,39 @@ class Node:
             middle += 1
             middle = int(middle/2)
             node.printNthNode(middle)
-                   
+
+    # sort a single linked list
+    @staticmethod
+    def sort(self):
+        if (self is None):
+            return self
+        min = self.val
+
+        # traverse list and swap as needed
+        node = self
+        while node.next != None:
+            print ("Node value:", node.val)
+            print ("Current min:", min)
+            # if (node.val < min):
+            #    Node.swap(self)         
+            if (node.val > node.next.val):
+                Node.swap(self)
+            node = node.next 
         
-             
+
+    # swaps positions of current node and its following node
+    @staticmethod
+    def swap(self):
+        if (self is None): return None
+        elif (self.next is None):
+            return self
+        else:
+            temp = self.next.val
+            self.next.val = self.val
+            self.val = temp
+        return self
+        
+  
 def main():
     """ Main entry point of the app """
     print("hello world")
@@ -68,18 +120,23 @@ def main():
     node1.next = node2 # 12->99
     node2.next = node3 # 99->37
     node3.next = node4 # 37->66
-    # node4.next = node5 # 66->27
+    node4.next = node5 # 66->27
     # the entire linked list now looks like: 12->99->37->66->27
     # node1.traverse()
     # node1.getListLength()
     # node1.printNthNode(3)
-    node1.getListMiddle()
+    # node1.getListMiddle()
+    #Node.traverseMain(node1)
 
-    # Rounded tests
-    '''decimal = 5/2
-    print("Decimal:", decimal)
-    rounded = round(decimal, )
-    print("Rounded:", rounded)'''
+
+    #Node.traverse(node1)
+    #Node.swap(node1)
+    #Node.traverse(node1)
+
+    Node.traverse(node1)
+    Node.sort(node1)
+    Node.traverse(node1)
+    
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
